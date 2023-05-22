f982a63ff8c7f63ae4090257c6e85b57b4991c5f
Shoban Chiddarth <messageshoban@gmail.com>
2023-05-22 10:43:53 -0700

    Updated the algorithm
    and the code

--- main.py
+++ main.py
@@ -1,26 +1,20 @@
-import os, pickle
-# All the numbers are steps of '### Programming Logic used' in 'README.md'
+filePath = "counterfile.bin"
 
-file=os.path.join(os.getcwd(), 'counterfile.bin') # The link to binary file where value is stored
+try:
+    fh = open(filePath, 'rb')
+    count = int.from_bytes(fh.read()) + 1
+    fh.close()
+    with open(filePath, 'wb') as fh:
+        fh.write(count.to_bytes())
 
-if os.path.exists(file): # 1
-    counterfile=open(file, 'rb')
-    count=pickle.load(counterfile)
-else: # 2
-    count=0
-    counterfile=open(file, 'wb')
-    pickle.dump(count, counterfile)
+except FileNotFoundError:
+    fh = open(filePath, 'wb')
+    count = 1
+    fh.write(count.to_bytes())
+    fh.close()
 
-counterfile.close() # 3
-count+=1 # 4
-counterfile=open(counterfile.name, mode='rb+') # 5
-
-
-# 6
-pickle.dump(count, counterfile)
-
-counterfile.close() # 7
-
-# 8
-print('This file counts number of times it was run')
-print('count =', count, end='') # `end` is simply useless
+print('This script counts how many times it has been run.')
+if count<=1:
+    print("This script has been run", count, "time")
+else:
+    print("This script has been run", count, "times")
